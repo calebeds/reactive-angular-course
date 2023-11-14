897d34669a9e3a20d3f033390fd7dd2a92b141e6
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-11-14 10:20:54 -0300

    implement data modification in reactive style

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
+import { filter, tap } from "rxjs/operators";
 
 @Component({
   selector: "courses-card-list",
@@ -11,6 +12,9 @@ import { CourseDialogComponent } from "../course-dialog/course-dialog.component"
 export class CoursesCardListComponent {
   @Input()
   courses: Course[] = [];
+
+  @Output()
+  private coursesChanged = new EventEmitter();
 
   constructor(private dialog: MatDialog) {}
 
@@ -24,5 +28,13 @@ export class CoursesCardListComponent {
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((val) => !!val),
+        tap(() => this.coursesChanged.emit())
+      )
+      .subscribe();
   }
 }
